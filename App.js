fc7a169c31d34832b7c75b8dcbeb4c48cd27e809
Caio Lucas <ccaio.oliveira83@gmail.com>
2022-11-15 22:06:59 -0300

    Initial commit
    Generated by create-expo-app 1.1.4.

--- App.js
+++ App.js
@@ -10,8 +10,11 @@ export default function App() {
   );
 }
 
-const style = StyleSheet.create({
-  App: {
-      paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
-  }
-})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
